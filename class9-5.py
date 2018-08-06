4aa75d14440b76363423b2e7b30ea28bc93a652d
Spring <kuznetsoffar@ya.ru>
2018-08-06 16:22:42 +0300

    9-5

--- class9-5.py
+++ class9-5.py
@@ -5,6 +5,7 @@ class User():
         self.name = name
         self.surname = surname
         self.age = age
+        self.login_atemp = 0
 
     def user_info(self):
         print(self.name,' ',self.surname,' ',self.age)
@@ -12,9 +13,24 @@ class User():
     def great_user(self):
         print('Hello ',self.name)
 
+    def increment_login_atemp(self):
+        self.login_atemp+=1
+
+    def reset_login_atemp(self):
+        self.login_atemp = 0
 
 pavel = User('Pavel','Zobov',23)
 
 pavel.user_info()
 
 pavel.great_user()
+
+print(pavel.login_atemp)
+pavel.increment_login_atemp()
+print(pavel.login_atemp)
+
+pavel.increment_login_atemp()
+print(pavel.login_atemp)
+
+pavel.reset_login_atemp()
+print(pavel.login_atemp)
