45701e50145c4620d6885fb72e602466ce5127cf
Spring <kuznetsoffar@ya.ru>
2018-08-09 16:44:29 +0300

    ordered dict test

--- classes/class9-13.py
+++ classes/class9-13.py
@@ -1,1 +1,19 @@
 from collections import OrderedDict
+dict = OrderedDict()
+
+dict['one'] = 1
+dict['two'] = 2
+
+print(dict)
+for k,v in dict.items():
+	print(k,' ',v)
+
+dict = {}
+
+dict['one'] = 1
+dict['two'] = 2
+
+print(dict)
+
+for k,v in dict.items():
+	print(k,' ',v)
